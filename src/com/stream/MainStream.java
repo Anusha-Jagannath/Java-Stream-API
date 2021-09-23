93c2a2974b8a6b3aa1a9bb6426436a057385d650
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-23 11:29:32 +0530

    Added: welcome message

---
+++ src/com/stream/MainStream.java
@@ -0,0 +1,10 @@
+package com.stream;
+/*
+ * MainStream class demonstrates stream API
+ */
+public class MainStream {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to stream API program");
+	}
+}
