6b6879e10180620bdbfa9d1d5f47e1a845154ea8
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-23 11:48:10 +0530

    Added: code to convert integer to double using map stream

--- src/com/stream/MainStream.java
+++ src/com/stream/MainStream.java
@@ -2,6 +2,8 @@ package com.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * MainStream class demonstrates stream API
@@ -11,11 +13,16 @@ public class MainStream {
 	public static void main(String[] args) {
 		System.out.println("Welcome to stream API program");
 
-		//iterating list using stream API
+		// iterating list using stream API
 		List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		integers.stream().forEach(n -> {
 			System.out.println("Stream forEach value " + n);
 		});
+
+		// converting integer list to double list using map function
+		Function<Integer, Double> doubleFunction = Integer::doubleValue;
+		List<Double> doubleList = integers.stream().map(doubleFunction).collect(Collectors.toList());
+		System.out.println("Printing double list " + doubleList);
 
 	}
 }
